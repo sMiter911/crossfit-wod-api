1b06443b1d6ff99fe15f61da8cc75d75741f504d
Wandu <31390092+sMiter911@users.noreply.github.com>
2022-07-27 22:09:15 +0200

    API Documentation

---
+++ src/controllers/recordController.js
@@ -0,0 +1,23 @@
+const recordService = require("../services/recordService");
+
+const getRecordForWorkOut = (req, res) => {
+  const {
+    params: { workoutId },
+  } = req;
+  if (!workoutId) {
+    res.status(400).send({
+      status: "FAILED",
+      data: { error: "Parameter ':workoutId' can not be empty" },
+    });
+  }
+  try {
+    const record = recordService.getRecordForWorkout(workoutId);
+    res.send({ status: "OK", data: record });
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .send({ status: "FAILED", data: { error: error?.message || error } });
+  }
+};
+
+module.exports = { getRecordForWorkOut };
--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const apicache = require("apicache");
 // ADD EXTERNAL ROUTES HERE
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
+const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 //ADD EXTERNAL MIDDLEWARE HERE
+const cache = apicache.middleware;
 app.use(bodyParser.json());
 
+app.use(cache("1 minute"));
 app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => {
   console.log(`API Server is listening on port ${PORT}`);
+  V1SwaggerDocs(app, PORT);
 });
